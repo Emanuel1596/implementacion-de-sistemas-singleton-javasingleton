8e5b3af2d8bd38bcc53c2dcc763117da734bfd8c
Emanuel Villanueva García <emanuel676@comunidad.unam.mx>
2025-12-12 15:18:44 -0600

    Actualizar Main para usar Singleton

--- Main.java
+++ Main.java
@@ -1,13 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        // Different parts of the code create their own config...
-        AppConfig config1 = new AppConfig();
-        AppConfig config2 = new AppConfig();
 
-        config1.setTheme("Dark"); // Change setting in one object...
+        AppConfig config1 = AppConfig.getInstance();
+        AppConfig config2 = AppConfig.getInstance();
+
+        config1.setTheme("Dark");
+        config1.setLanguage("en-US");
 
         config1.printConfig();
-        config2.printConfig(); // ...but the other object is unaware! This is bad.
+        config2.printConfig();
 
         System.out.println("Are these the same instance? " + (config1 == config2));
     }
