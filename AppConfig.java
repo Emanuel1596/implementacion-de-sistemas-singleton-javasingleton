5ece3a4d4af663211a38abd631323d39508c6c46
Emanuel Villanueva García <emanuel676@comunidad.unam.mx>
2026-01-09 18:49:10 -0600

    Update AppConfig.java
    Hola

--- AppConfig.java
+++ AppConfig.java
@@ -1,21 +1,37 @@
 public class AppConfig {
+
+    // Instancia única, privada, estática y final
+    private static final AppConfig instance = new AppConfig();
+
+    // Configuraciones globales de la aplicación
     private String theme;
     private String language;
 
-    public AppConfig() {
-        // Load default settings
+    // 2️Constructor PRIVADO
+    private AppConfig() {
         this.theme = "Light";
-        this.language = "EN";
-        System.out.println("New AppConfig instance created!");
+        this.language = "ES";
     }
 
-    public String getTheme() { return theme; }
-    public void setTheme(String theme) { this.theme = theme; }
+    //  Método público para acceder a la instancia
+    public static AppConfig getInstance() {
+        return instance;
+    }
 
-    public String getLanguage() { return language; }
-    public void setLanguage(String language) { this.language = language; }
+    // Métodos getter y setter
+    public String getTheme() {
+        return theme;
+    }
 
-    public void printConfig() {
-        System.out.println("Theme: " + theme + ", Language: " + language);
+    public void setTheme(String theme) {
+        this.theme = theme;
+    }
+
+    public String getLanguage() {
+        return language;
+    }
+
+    public void setLanguage(String language) {
+        this.language = language;
     }
 }
